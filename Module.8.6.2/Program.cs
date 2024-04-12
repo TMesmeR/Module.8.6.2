58481ec1939153e853c3268b25d079170e49a455
TMesmeR <mesmerat@gmail.com>
2024-04-12 15:59:19 +0300

    Добавьте файлы проекта.

---
+++ Module.8.6.2/Program.cs
@@ -0,0 +1,32 @@
+﻿string directoryPath = "Folder";
+
+if (!Directory.Exists(directoryPath))
+{
+    Console.WriteLine("Указанная папка не существует");
+    return;
+}
+
+long size = GetDirectorySize(directoryPath);
+Console.WriteLine($"Размер папки : {size} ");
+
+long GetDirectorySize(string directoryPath)
+{
+    long size = 0;
+
+    
+    string[] fileNames = Directory.GetFiles(directoryPath);
+    foreach (string fileName in fileNames)
+    {
+        FileInfo fileInfo = new FileInfo(fileName);
+        size += fileInfo.Length;
+    }
+
+    
+    string[] subDirectoryNames = Directory.GetDirectories(directoryPath);
+    foreach (string subDirectoryName in subDirectoryNames)
+    {
+        size += GetDirectorySize(subDirectoryName);
+    }
+
+    return size;
+}
